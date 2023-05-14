933324bd745f5cef7b43a87c46f7d6e785eed656
Adrien Poupa <adrien@poupa.net>
2023-05-14 00:19:04 -0400

    feat: replace Heimdall by Homepage

--- update-config.sh
+++ update-config.sh
@@ -8,6 +8,7 @@ do
   sleep 5
 done
 sed -i.bak "s/<UrlBase><\/UrlBase>/<UrlBase>\/radarr<\/UrlBase>/" ./radarr/config.xml && rm ./radarr/config.xml.bak
+sed -i.bak 's/^RADARR_API_KEY=.*/RADARR_API_KEY='"$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' ./radarr/config.xml)"'/' .env && rm .env.bak
 
 echo "Updating Sonarr configuration..."
 until [ -f ./sonarr/config.xml ]
@@ -15,6 +16,7 @@ do
   sleep 5
 done
 sed -i.bak "s/<UrlBase><\/UrlBase>/<UrlBase>\/sonarr<\/UrlBase>/" ./sonarr/config.xml && rm ./sonarr/config.xml.bak
+sed -i.bak 's/^SONARR_API_KEY=.*/SONARR_API_KEY='"$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' ./sonarr/config.xml)"'/' .env && rm .env.bak
 
 echo "Updating Prowlarr configuration..."
 until [ -f ./prowlarr/config.xml ]
@@ -22,6 +24,7 @@ do
   sleep 5
 done
 sed -i.bak "s/<UrlBase><\/UrlBase>/<UrlBase>\/prowlarr<\/UrlBase>/" ./prowlarr/config.xml && rm ./prowlarr/config.xml.bak
+sed -i.bak 's/^PROWLARR_API_KEY=.*/PROWLARR_API_KEY='"$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' ./prowlarr/config.xml)"'/' .env && rm .env.bak
 
 echo "Updating Jellyfin configuration..."
 until [ -f ./jellyfin/network.xml ]
