a0af1d7960211c2fb3ff2f4c07058c294067a8ad
Adrien Poupa <15086425+AdrienPoupa@users.noreply.github.com>
2024-05-12 23:47:15 -0400

    fix(config): #70 Make script Bash 4 compatible by removing upper case operator

--- update-config.sh
+++ update-config.sh
@@ -3,25 +3,26 @@
 # See https://stackoverflow.com/a/44864004 for the sed GNU/BSD compatible hack
 
 function update_arr_config {
-  echo "Updating ${container^} configuration..."
+  echo "Updating ${container} configuration..."
   until [ -f "${CONFIG_ROOT:-.}"/"$container"/config.xml ]; do sleep 1; done
   sed -i.bak "s/<UrlBase><\/UrlBase>/<UrlBase>\/$1<\/UrlBase>/" "${CONFIG_ROOT:-.}"/"$container"/config.xml && rm "${CONFIG_ROOT:-.}"/"$container"/config.xml.bak
-  sed -i.bak 's/^'"${container^^}"'_API_KEY=.*/'"${1^^}"'_API_KEY='"$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' "${CONFIG_ROOT:-.}"/"$container"/config.xml)"'/' .env && rm .env.bak
-  echo "Update of ${container^} configuration complete, restarting..."
+  CONTAINER_NAME_UPPER=$(echo "$container" | tr '[:lower:]' '[:upper:]')
+  sed -i.bak 's/^'"${CONTAINER_NAME_UPPER}"'_API_KEY=.*/'"${CONTAINER_NAME_UPPER}"'_API_KEY='"$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' "${CONFIG_ROOT:-.}"/"$container"/config.xml)"'/' .env && rm .env.bak
+  echo "Update of ${container} configuration complete, restarting..."
   docker compose restart "$container"
 }
 
 function update_qbittorrent_config {
-    echo "Updating ${container^} configuration..."
+    echo "Updating ${container} configuration..."
     docker compose stop "$container"
     until [ -f "${CONFIG_ROOT:-.}"/"$container"/qBittorrent/qBittorrent.conf ]; do sleep 1; done
     sed -i.bak '/WebUI\\ServerDomains=*/a WebUI\\Password_PBKDF2="@ByteArray(ARQ77eY1NUZaQsuDHbIMCA==:0WMRkYTUWVT9wVvdDtHAjU9b3b7uB8NR1Gur2hmQCvCDpm39Q+PsJRJPaCU51dEiz+dTzh8qbPsL8WkFljQYFQ==)"' "${CONFIG_ROOT:-.}"/"$container"/qBittorrent/qBittorrent.conf && rm "${CONFIG_ROOT:-.}"/"$container"/qBittorrent/qBittorrent.conf.bak
-    echo "Update of ${container^} configuration complete, restarting..."
+    echo "Update of ${container} configuration complete, restarting..."
     docker compose start "$container"
 }
 
 function update_bazarr_config {
-    echo "Updating ${container^} configuration..."
+    echo "Updating ${container} configuration..."
     until [ -f "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml ]; do sleep 1; done
     sed -i.bak "s/base_url: ''/base_url: '\/$container'/" "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml && rm "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml.bak
     sed -i.bak "s/use_radarr: false/use_radarr: true/" "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml && rm "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml.bak
@@ -32,8 +33,8 @@ function update_bazarr_config {
     until [ -f "${CONFIG_ROOT:-.}"/radarr/config.xml ]; do sleep 1; done
     RADARR_API_KEY=$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' "${CONFIG_ROOT:-.}"/radarr/config.xml)
     sed -i.bak "/radarr:/,/^sonarr:/ { s/apikey: .*/apikey: $RADARR_API_KEY/; s/base_url: .*/base_url: \/radarr/; s/ip: .*/ip: radarr/ }" "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml && rm "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml.bak
-    sed -i.bak 's/^'"${container^^}"'_API_KEY=.*/'"${container^^}"'_API_KEY='"$(sed -n 's/.*apikey: \(.*\)*/\1/p' "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml | head -n 1)"'/' .env && rm .env.bak
-    echo "Update of ${container^} configuration complete, restarting..."
+    sed -i.bak 's/^BAZARR_API_KEY=.*/BAZARR_API_KEY='"$(sed -n 's/.*apikey: \(.*\)*/\1/p' "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml | head -n 1)"'/' .env && rm .env.bak
+    echo "Update of ${container} configuration complete, restarting..."
     docker compose restart "$container"
 }
 
